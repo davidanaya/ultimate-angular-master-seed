b273346cfd9c1b56fa31a86b33969442d5e3c8be
David Anaya <danaya@gmail.com>
2016-09-06 19:54:11 +0200

    login/register routes.v3

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,21 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyB0HEHm8WnAQxNeF7ctEvGiUaPjCGwD2Wk",
+      authDomain: "contacts-manager-d5c92.firebaseapp.com",
+      databaseURL: "https://contacts-manager-d5c92.firebaseio.com",
+      storageBucket: "contacts-manager-d5c92.appspot.com",
+    };
+
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      });
+      
+    firebase.initializeApp(config);
+  });
