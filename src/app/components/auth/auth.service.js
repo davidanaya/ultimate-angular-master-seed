60e0dcc175e609b23ad6ef6007145721be48fb9a
BME\danaya <david.anaya@atos.net>
2016-09-08 16:20:00 +0200

    v4. auth redirects and core app

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -5,6 +5,13 @@ function AuthService($firebaseAuth) {
     authData = response;
     return authData;    
   }
+  function onSignIn(user) {
+    authData = user;
+    return auth.$requireSignIn();
+  }
+  function clearAuthData() {
+    authData = null;
+  }
   this.login = function(user) {
     return auth
       .$signInWithEmailAndPassword(user.email, user.password)
@@ -14,6 +21,23 @@ function AuthService($firebaseAuth) {
     return auth
       .$createUserWithEmailAndPassword(user.email, user.password)
       .then(storeAuthData);
+  }
+  this.logout = function() {
+    return auth
+      .$signOut()
+      .then(clearAuthData);
+  }
+  this.requireAuthentication = function() {
+    return auth
+      .$waitForSignIn().then(onSignIn);
+  }
+  this.isAuthenticated = function() {
+    return !!authData;
+  }
+  this.getUser = function() {
+    if (authData) {
+      return authData;
+    }
   }
 }
 
